7882f944af4f2ec7ad6afe917970cab93d9e1ac4
Ivan <ivanprivalov@mail.ru>
2020-03-11 01:23:08 +0300

    название коммита

--- js/filter.js
+++ js/filter.js
@@ -8,14 +8,50 @@
     return Math.floor(Math.random() * (array.length - MAX_ADS_TO_DISPLAY - 1));
   };
 
-  var getFilterPropertyType = function () {
+  var getHousingType = function () {
     return document.querySelector('#housing-type').value;
   };
+
+  var getHousingPrice = function () {
+    return document.querySelector('#housing-price').value;
+  };
+
+  var getHousingGuests = function () {
+    return document.querySelector('#housing-guests').value;
+  };
+
+  var getHousingFeatures = function () {
+    var array = [];
+    document.querySelectorAll('#housing-features input:checked').forEach(function (it) {
+      array.push(it.value);
+    });
+    return array;
+  };
+
+  var getRank = function (property) {
+    var rank = 0;
+    if (property.offer.type === getHousingType()) {
+      rank++;
+    }
+    switch(getHousingPrice()) {
+      case ('middle'):
+        if (property.offer.price >= 10000 && property.offer.price < 50000) {
+          rank++;
+        }
+        break;
+      case ('low'):
+        if (property.offer.price < 10000) {
+          rank++;
+        }
+    }
+  };
+
+  console.log(getHousingPrice());
 
   window.filterProperties = function (array) {
 
     var filteredProperties = array.filter(function (element) {
-      return element.offer.type === getFilterPropertyType();
+      return element.offer.type === getFilterPropertyType(); // housing type
     });
 
     if (filteredProperties.length <= MAX_ADS_TO_DISPLAY) {
